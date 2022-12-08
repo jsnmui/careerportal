8c99d685f298d14d23b702b84913a1f23edfea03
pdfaerber <87340230+pdfaerber@users.noreply.github.com>
2022-12-08 17:46:54 -0500

    Testing changes to routing by adding switch  in app.js Commented out original code for now.

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,21 +1,36 @@
-
+import React from "react";
+import { ReactDOM } from "react";
 import './App.css';
-import Dropdown from './Components/Dropdown';
+// import Dropdown from './Components/Dropdown';
 import JobPost from './Components/JobPost';
 import NavBar from './Components/NavBar';
-import { Post } from './Components/Post';
-import Options from './MockInfo/Options';
+// import { Post } from './Components/Post';
+// import Options from './MockInfo/Options';
+
+import { Route, Switch } from "react-router-dom";
 // import './Styles/JobPost.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-        <NavBar/>
-        <JobPost/>
-        <Post/>
-        {/* <Dropdown placeHolder="Location" options ={Options}/> */}
-    </div>
+    <div>
+      <NavBar/>
+      <Switch>
+        <Route path="/jobs" exact component={JobPost}/>
+        </Switch>
+     
+        </div>
   );
-}
+};
+
 
 export default App;
+// function App() {
+//   return (
+//     <div className="App">
+//         
+//         <JobPost/>
+//         <Post/>
+//         {/* <Dropdown placeHolder="Location" options ={Options}/> */}
+//     </div>
+//   );
+// }
