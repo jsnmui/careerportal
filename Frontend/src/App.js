45b42ff21858c19b34f44cff1862148016f97e5b
pdfaerber <87340230+pdfaerber@users.noreply.github.com>
2022-12-09 10:16:12 -0500

    committing changes from ui to DB testing. No material changes.

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,36 +1,45 @@
 import React from "react";
+
+//react routing
 import { ReactDOM } from "react";
-import './App.css';
-// import Dropdown from './Components/Dropdown';
+// import { Route, Switch } from "react-router-dom";
+
+//components
+import Dropdown from './Components/Dropdown';
 import JobPost from './Components/JobPost';
 import NavBar from './Components/NavBar';
-// import { Post } from './Components/Post';
-// import Options from './MockInfo/Options';
+import { Post } from './Components/Post';
+import Options from './MockInfo/Options';
 
-import { Route, Switch } from "react-router-dom";
+import './App.css';
+import { Component } from "react";
 // import './Styles/JobPost.css';
 
 const App = () => {
   return (
-    <div>
-      <NavBar/>
-      <Switch>
-        <Route path="/jobs" exact component={JobPost}/>
-        </Switch>
-     
+        <div className="App">
+            
+            <JobPost/>
+            <Post/>
+            <Dropdown placeHolder="Location" options ={Options}/>
         </div>
-  );
+      );
+  
 };
 
 
 export default App;
 // function App() {
-//   return (
-//     <div className="App">
-//         
-//         <JobPost/>
-//         <Post/>
-//         {/* <Dropdown placeHolder="Location" options ={Options}/> */}
-//     </div>
-//   );
+//  
 // }
+
+// return (
+//   <div>
+//     <NavBar/>
+//     <Switch>
+// <route path "/" exact Component = {}>
+//       <Route path="/jobs" exact component={JobPost}/>
+//       </Switch>
+   
+//       </div>
+// );
