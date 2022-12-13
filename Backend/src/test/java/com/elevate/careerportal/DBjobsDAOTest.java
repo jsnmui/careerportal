365443732aeb987f58716cc3f9ba45962993ad26
Jason Mui <jasonmui2004@gmail.com>
2022-12-13 15:47:34 -0500

    experiment with update

---
+++ Backend/src/test/java/com/elevate/careerportal/DBjobsDAOTest.java
@@ -0,0 +1,36 @@
+package com.elevate.careerportal;
+
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+class DBjobsDAOTest {
+
+      DBjobsDAO dao =  new DBjobsDAO();
+
+    @Mock
+    private Jobs jobs;
+
+    @MockBean
+    private JdbcTemplate template;
+
+    @Before
+    public void setUp() {
+      template =mock(JdbcTemplate.class);
+        jobs=mock(Jobs.class);
+        ReflectionTestUtils.setField(dao, "template", template);
+    }
+
+
+    @Test
+    void addJob() {
+
+    }
+}
