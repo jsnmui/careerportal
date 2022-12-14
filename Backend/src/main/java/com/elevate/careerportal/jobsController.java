63fb790a5e05b4b4df6bf51a5e9c46ae4edd94c8
Jason Mui <jasonmui2004@gmail.com>
2022-12-14 12:57:50 -0500

    added validation for locationid,departmentid and userid

--- Backend/src/main/java/com/elevate/careerportal/jobsController.java
+++ Backend/src/main/java/com/elevate/careerportal/jobsController.java
@@ -3,6 +3,8 @@ package com.elevate.careerportal;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 import java.util.Collection;
 
 @CrossOrigin(origins = "http://localhost:3000", methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.DELETE, RequestMethod.PUT})
@@ -32,7 +34,7 @@ public class jobsController {
 
 
     @PostMapping(value="/jobs")
-    public Jobs addJob(@RequestBody Jobs jobs) {
+    public Jobs addJob(@Valid @RequestBody Jobs jobs) {
         return this.jobs.addJob(jobs);
     }
 //
