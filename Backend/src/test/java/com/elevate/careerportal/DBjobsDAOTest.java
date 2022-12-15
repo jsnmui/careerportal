2b9319dd133888a1eff237566ba5a5584b7da464
Jason Mui <jasonmui2004@gmail.com>
2022-12-15 08:59:02 -0500

    added validation for locationid,departmentid and userid

--- Backend/src/test/java/com/elevate/careerportal/DBjobsDAOTest.java
+++ Backend/src/test/java/com/elevate/careerportal/DBjobsDAOTest.java
@@ -5,34 +5,31 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.jdbc.DataJdbcTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.util.ReflectionTestUtils;
 
 import java.time.LocalDateTime;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
-
+@WebMvcTest(DBjobsDAO.class)
+@ContextConfiguration(locations="classpath:testbeans.xml")
 class DBjobsDAOTest {
 
-      DBjobsDAO dao =  new DBjobsDAO();
-
-     @Mock
-    private Jobs jobs;
-
-   @Mock
-    private JdbcTemplate template;
-
-    @BeforeEach
-    public void setUp() {
-
-        ReflectionTestUtils.setField(dao, "template", template);
-    }
-
-
+    @Mock
+    JdbcTemplate template;
     @Test
-    void addJob() {
-
-    }
+   public void whenMockJdbc_thenReturnJobId1(){
+                DBjobsDAO j = new DBjobsDAO();
+               Jobs job = new Jobs(1, "Job", "Job Description", 12, LocalDateTime.parse("2022-12-10T05:00:00")
+                                , LocalDateTime.parse("2022-12-10T05:00:00")
+                               , null, null, null, null, 21);
+               ReflectionTestUtils.setField(j, "template", template);
+               when(template.queryForObject(anyString(), any(RowMapper.class), any())).thenReturn(job);
+                assertEquals(job, j.getById(1));
+           }
 }
