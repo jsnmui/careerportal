5a7b52577bc28a7759c88c2f55b719f72f651a1b
Jason Mui <jasonmui2004@gmail.com>
2022-12-15 09:10:39 -0500

    got rid of unrelated code

--- Backend/src/main/java/com/elevate/careerportal/jobsController.java
+++ Backend/src/main/java/com/elevate/careerportal/jobsController.java
@@ -37,10 +37,6 @@ public class jobsController {
     public Jobs addJob(@Valid @RequestBody Jobs jobs) {
         return this.jobs.addJob(jobs);
     }
-//
-//    @PutMapping(value="/archive")
-//    void update(@RequestBody Jobs jobs) {
-//        this.jobs.update(jobs);
-//    }
+
 
 }
