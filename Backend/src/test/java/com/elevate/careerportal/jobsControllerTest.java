f186917024db941ffed90913080f503e8ef6d460
Liubov Sariban <liubov.sariban@peopleshores.net>
2022-12-22 14:34:26 -0500

    all tests are working

--- Backend/src/test/java/com/elevate/careerportal/jobsControllerTest.java
+++ Backend/src/test/java/com/elevate/careerportal/jobsControllerTest.java
@@ -1,8 +1,6 @@
 package com.elevate.careerportal;
 
-
 import org.hamcrest.Matchers;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,15 +9,11 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-
 import java.time.LocalDateTime;
-import java.util.regex.Matcher;
-
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -56,7 +50,7 @@ public class jobsControllerTest {
     @Test
     void postNewJob(){
         Jobs newJob = new Jobs(11,"Intern Software engineer",
-                "Write code, learn , solve problems , profit", 1, LocalDateTime.parse("2022-12-10T05:00:00")
+                "Write code, learn , solve problems , profit", "1", LocalDateTime.parse("2022-12-10T05:00:00")
                 ,LocalDateTime.parse("2022-12-31T05:00:00"),
                 true,400,1000,"2. Clarksdale",3);
 
@@ -67,7 +61,7 @@ public class jobsControllerTest {
                             .content("""
                             {
                                 "userId" : 3,
-                                "departmentId": 1,
+                                "departmentId": "1",
                                 "locationId": "2. Clarksdale",
                                 "jobTitle" : "Intern Software engineer",
                                 "jobDescription" :  "Write code, learn , solve problems , profit",
@@ -84,7 +78,7 @@ public class jobsControllerTest {
                     .andExpect(MockMvcResultMatchers.jsonPath("jobId").value(11))
                     .andExpect(MockMvcResultMatchers.jsonPath("jobTitle").value("Intern Software engineer"))
                     .andExpect(MockMvcResultMatchers.jsonPath("jobDescription").value("Write code, learn , solve problems , profit"))
-                    .andExpect(MockMvcResultMatchers.jsonPath("departmentId").value(1))
+                    .andExpect(MockMvcResultMatchers.jsonPath("departmentId").value("1"))
                     .andExpect(MockMvcResultMatchers.jsonPath("locationId").value(2))
                     .andExpect(jsonPath("$.*",Matchers.hasSize(11)))
                     .andExpect(MockMvcResultMatchers.jsonPath("userId").value(3))
@@ -94,7 +88,6 @@ public class jobsControllerTest {
                     .andExpect(MockMvcResultMatchers.jsonPath("maxSal").value(1000))
                     .andExpect(MockMvcResultMatchers.jsonPath("isActive").value(true))
                     .andExpect(status().isOk()).andReturn();
-
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
